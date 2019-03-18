ae1a2413f91e1ff64078e730e16e9996820b9173
ballysharnal <46532141+ballysharnal@users.noreply.github.com>
2019-03-18 22:20:53 +0100

    Update obj.js

--- exercises/obj.js
+++ exercises/obj.js
@@ -17,17 +17,26 @@ let obj = {
 //* Begin of tests
 const assert = require('assert')
 
-console.log(typeof obj, 'object')
 
-console.log(typeof obj.str, 'string')
-console.log(obj.str, '1337')
+assert.strictEqual(typeof obj, 'object')
 
-console.log(typeof obj.num, 'number')
-console.log(obj.num, 42)
 
-console.log(typeof obj.bool, 'boolean')
-console.log(obj.bool, false)
+assert.strictEqual(typeof obj.str, 'string')
 
-console.log(typeof obj.undef, 'undefined')
-console.log(obj.undef, undefined)
+assert.strictEqual(obj.str, '1337')
+
+
+assert.strictEqual(typeof obj.num, 'number')
+
+assert.strictEqual(obj.num, 42)
+
+
+assert.strictEqual(typeof obj.bool, 'boolean')
+
+assert.strictEqual(obj.bool, false)
+
+
+assert.strictEqual(typeof obj.undef, 'undefined')
+
+assert.strictEqual(obj.undef, undefined)
 // End of tests */
